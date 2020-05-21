4aa3c4dc289482e62a0d7a604d72e80d97df6000
LCLP <lukas@lclpnet.work>
2020-05-21 14:15:11 +0200

    init

---
+++ ServerBase/src/main/java/work/lclpnet/serverbase/ServerBase.java
@@ -0,0 +1,66 @@
+package work.lclpnet.serverbase;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.custom.ServerReloadedEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppedEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import work.lclpnet.core.util.ComponentSupplier;
+
+@Mod(ServerBase.MODID)
+public class ServerBase {
+	
+	public static final String MODID = "serverbase";
+	private static final Logger LOGGER = LogManager.getLogger();
+	public static final ComponentSupplier TEXT = new ComponentSupplier("ServerBase");
+
+	public ServerBase() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+		IEventBus bus = MinecraftForge.EVENT_BUS;
+		bus.register(this);
+	}
+
+	private void setup(final FMLCommonSetupEvent event) { //preinit
+		LOGGER.info("ServerBase initializing...");
+
+		LOGGER.info("ServerBase initialized.");
+	}
+
+	@SubscribeEvent
+	public void onServerStarting(FMLServerStartingEvent e) {
+		LOGGER.info("ServerBase starting...");
+	}
+
+	@SubscribeEvent
+	public void onServerStarted(FMLServerStartedEvent e) {
+		LOGGER.info("ServerBase started.");
+	}
+	
+	@SubscribeEvent
+	public void onServerStop(FMLServerStoppingEvent e) {
+		LOGGER.info("ServerBase stopping...");
+	}
+	
+	@SubscribeEvent
+	public void onServerStopped(FMLServerStoppedEvent e) {
+		LOGGER.info("ServerBase stopped.");
+	}
+
+	@SubscribeEvent
+	public void onServerReloaded(ServerReloadedEvent e) {
+		LOGGER.info("ServerBase reloading...");
+
+		LOGGER.info("ServerBase reloaded.");
+	}
+	
+}
